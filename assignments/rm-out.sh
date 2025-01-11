d38095175efe81c3ab188b586d86ed93b517e10c
finyle <linshihong96@gmail.com>
2025-01-11 23:07:07 +0800

    successful

---
+++ assignments/rm-out.sh
@@ -0,0 +1,7 @@
+
+cd PA2 && make clean && echo "2over\n" && cd ..
+cd PA3 && make clean && echo "3over\n" && cd ..
+cd PA4 && make clean && echo "4over\n" && cd ..
+cd PA5 && make clean && echo "5over\n"
+
+
